7730ee75c7728d259cd11fbf9aa043888c43bc44
Maksud <leylakarimova@mail.ru>
2023-03-28 23:23:56 +0300

    Added carousel object and fixed wrappers placement

---
+++ src/Carousel/HousesCarousel.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { House } from './houseComp';
+import houses_fillings from './houses';
+import { useState, useEffect } from 'react';
+import './HousesCarousel.css';
+import arrow from '../images/icons/right-arrow.svg'
+
+export const HousesCarousel = ({children}) => {
+
+    const [houses, setHouses] = useState([])
+    const [offset, setOffset] = useState(0)
+    const houseDealWIDTH = 305;
+    useEffect(() => {
+        setHouses(houses.concat(houses_fillings))
+    }, [])
+
+    const handleLeftArrowClick = () => {
+        setOffset((currentOffset) => {
+            const newOffset = currentOffset + houseDealWIDTH
+            
+            return Math.min(newOffset, 0)
+        })
+    }
+    
+    const handleRightArrowClick = () => {
+        setOffset((currentOffset) => {
+            const newOffset = currentOffset - houseDealWIDTH
+            const maxOffset = -(houseDealWIDTH * (houses.length - 3))
+            return Math.max(newOffset, maxOffset)
+        })
+    }
+
+    return (
+        <div className='HousesCarousel'>
+            <button class='CarouselButton LeftButton' onClick={handleLeftArrowClick}>
+                <img className="Arrow LeftArrow" src={arrow}/>
+            </button>
+            <div className='HousesWindow'>
+                <div className='HousesWrapper'
+                    style={{
+                        transform: `translateX(${offset}px)`,
+                    }}>
+                {
+                  houses.map((house, index) => {
+                    return <House house={house} key={index}></House>
+                  })}
+                </div>
+            </div>
+            <button class='CarouselButton RightButton' onClick={handleRightArrowClick}>
+                <img className="Arrow RightArrow" src={arrow}/>
+            </button>
+        </div>
+    )
+}
---
+++ src/Carousel/houses.js
@@ -0,0 +1,14 @@
+import rise from '../images/houses/rise.jpg'
+import rest from '../images/houses/rest.jpg'
+import heavens_pleasure from '../images/houses/heavens_pleasure.jpg'
+import black_house from '../images/houses/black_house.jpg'
+import cool_hotel from '../images/houses/cool_hotel.jpg'
+
+const houses_fillings = [
+    {imageSource: rise, cost: "50 млн руб.", owner: "ЖК \"Рассвет\"", region: "г. Москва"},
+    {imageSource: rest, cost: "44 млн руб.", owner: "ЖК \"Райское наслаждение\"", region: "г. Краснодар"},
+    {imageSource: heavens_pleasure, cost: "22 млн руб.", owner: "Апарт-отель \"Отдых\"", region: "г. Сочи"},
+    {imageSource: black_house, cost: "40 млн руб.", owner: "Апарт-отель \"Элита\"", region: "г. Сочи"},
+    {imageSource: cool_hotel, cost: "35 млн руб.", owner: "Агенство \"Биливинг\"", region: "г. Новгород"}
+];
+export default houses_fillings;
